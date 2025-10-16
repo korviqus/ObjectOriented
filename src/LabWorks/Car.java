e9bac830bd77b267baeb5c203d74ca6a4bdc8182
OMORI <ozangacanozan@gmail.com>
2025-10-16 15:16:48 +0300

    Update

---
+++ src/LabWorks/Car.java
@@ -0,0 +1,24 @@
+package LabWorks;
+import java.util.*;
+
+public class Car {
+
+    String brand;
+    String model;
+    int year;
+    Engine engine;
+
+    public Car(String brand, String model, int year, int horsepowr){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayInfo(){
+        System.out.println(this.year+ " " + this.brand+ " " + this.model);
+    }
+    public static void main(String[] args){
+        Car myCar = new Car("BMW","i8","2018");
+        myCar.displayInfo();
+    }
+}
---
+++ src/LabWorks/Engine.java
@@ -0,0 +1,14 @@
+package LabWorks;
+
+public class Engine{
+
+    enum FuelTYpe{
+        GASOLINE, DIESEL, ELECTRIC;
+
+    int hp;
+    public Engine(int horsepower){
+        this.hp = horsepower;
+    }
+
+    }
+}
